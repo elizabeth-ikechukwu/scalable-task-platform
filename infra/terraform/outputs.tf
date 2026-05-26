e2db35042c9e6bf6ff4c4f2d0f9c7c7dcd9e7649
ikechukwu Elizabeth <elizabethik135@gmail.com>
2026-05-26 07:34:04 +0100

    week-7: fix private subnet index reference in outputs

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -10,7 +10,7 @@ output "public_subnet_id" {
 
 output "private_subnet_id" {
   description = "ID of the private subnet"
-  value       = aws_subnet.private.id
+  value       = aws_subnet.private[0].id
 }
 
 output "ec2_instance_id" {
